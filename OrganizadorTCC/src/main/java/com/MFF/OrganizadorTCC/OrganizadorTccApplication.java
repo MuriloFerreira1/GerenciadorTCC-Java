b3065f0738217c1adbd11ee9eab48e3e43ecd709
Murilo Ferreira <murilo@ive.local>
2024-05-14 13:22:16 -0300

    commit inicial

---
+++ OrganizadorTCC/src/main/java/com/MFF/OrganizadorTCC/OrganizadorTccApplication.java
@@ -0,0 +1,13 @@
+package com.MFF.OrganizadorTCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizadorTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizadorTccApplication.class, args);
+	}
+
+}
