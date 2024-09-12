4ccc8d6d94273831ec5b0343a10160473a7e1761
Murilo Ferreira <rillofelix@hotmail.com>
2024-09-12 11:29:28 -0300

    inserção de usuarios para demonstração

---
+++ src/main/resources/db/migration/V6__Demo_Usuarios.sql
@@ -0,0 +1,5 @@
+INSERT INTO `alunos` (`id`, `RM`, `CPF`, `email`, `senha`, `projeto_id`, `nome`, `curso`, `turma`) VALUES 
+	(NULL, '2', '12345678910', 'suporte@ivecompany.com.br', '{bcrypt}$2a$10$tkVn3XuKZUvkZV85s4ry3Oc.4OQAii/65ck1yF81LwiYBxVO4NDy6', '4', 'Murilo', 'Análise e Desenvolvimento de Sistemas', '20222');
+	
+INSERT INTO `professores` (`id`, `RM`, `CPF`, `email`, `senha`, `nome`, `curso`, `organizador`) VALUES 
+	(NULL, '1', '0', 'rillofelix@gmail.com', '{bcrypt}$2a$10$tkVn3XuKZUvkZV85s4ry3Oc.4OQAii/65ck1yF81LwiYBxVO4NDy6', 'Cristina', 'Análise e Desenvolvimento de Sistemas', '1')
